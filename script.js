f7d522da44a08d996d43a04b9ddfbf603458c57a
barbatov <jack.775hotmail.com@gmail.com>
2026-05-04 15:41:40 -0300

    adicionado logica para embaralhar as cartas

---
+++ script.js
@@ -0,0 +1,33 @@
+// variáveis de estado de jogo.
+
+let flippedCards = [] // Array que armazena as cartas viradas(sempre terá no máximo duas).
+let matchedPairs = 0 // contador de pares encontrados.
+let attempts = 0 // contador de tentativas.
+let isCheckingPair = false // Trava o jogo enquanto verifica o par ou esconde as cartas.
+
+// Array com todas as cartas do jogo
+const cardItems = [
+    {id: 1, content: "👓", matched: false},
+    {id: 2, content: "👓", matched: false},
+    {id: 3, content: "👄", matched: false},
+    {id: 4, content: "👄", matched: false},
+    {id: 5, content: "💩", matched: false},
+    {id: 6, content: "💩", matched: false},
+    {id: 7, content: "👽", matched: false},
+    {id: 8, content: "👽", matched: false},
+    {id: 9, content: "🚀", matched: false},
+    {id: 10, content: "🚀", matched: false},
+    {id: 11, content: "🍰", matched: false},
+    {id: 12, content: "🍰", matched: false},
+    {id: 13, content: "🏯", matched: false},
+    {id: 14, content: "🏯", matched: false},
+    {id: 15, content: "♣️", matched: false},
+    {id: 16, content: "♣️", matched: false},
+]
+
+function shuffleCards(array){
+    const shuffled = array.sort(() => (Math.random() > 0.5 ? 1 : -1))
+    console.log(shuffled)
+}
+
+shuffleCards(cardItems) // Embaralha as cartas antes de iniciar o jogo.
